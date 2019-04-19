beb109950367dbaa622d7af6515911c2e0690df7
gguifroes <gui_froes@hotmail.com>
2019-04-19 00:21:52 -0300

    Commit AC 4 e 5

---
+++ AplicacaoDjango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def nintendoSwitch(request):
+	return render(request,"nintendoSwitch.html")
+def ps4(request):
+	return render(request,"ps4.html")
+def index(request):
+	return render(request,"index.html")
+def login(request):
+	return render(request,"login.html")
+def xboxOne(request):
+	return render(request,"xboxOne.html")
